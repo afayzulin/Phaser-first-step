b5b5473cbd4f9b43d05d7d0f99bb2d381e54d291
Artyom <artyom.fayzulin@gmail.com>
2018-09-16 13:55:16 +0300

    tab

--- js/main.js
+++ js/main.js
@@ -132,22 +132,22 @@ function collectStar (player, star)
     score += 10;
     scoreText.setText('Score: ' + score);
     if (stars.countActive(true) === 0)
-        {
-            stars.children.iterate(function (child) {
+    {
+        stars.children.iterate(function (child) {
 
-                child.enableBody(true, child.x, 0, true, true);
+            child.enableBody(true, child.x, 0, true, true);
 
-            });
+        });
 
-            var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
 
-            var bomb = bombs.create(x, 16, 'bomb');
-            bomb.setBounce(1);
-            bomb.setCollideWorldBounds(true);
-            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-            bomb.allowGravity = false;
+        var bomb = bombs.create(x, 16, 'bomb');
+        bomb.setBounce(1);
+        bomb.setCollideWorldBounds(true);
+        bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        bomb.allowGravity = false;
 
-        }
+    }
 }
 
 function hitBomb (player, bomb)
