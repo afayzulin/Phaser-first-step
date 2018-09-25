a1a7c9e7908592030e0dd41a98c14d539f2c6e31
Artyom Fayzulin <43291819+afayzulin@users.noreply.github.com>
2018-09-25 12:25:43 +0300

    formated if "style"

--- js/main.js
+++ js/main.js
@@ -98,27 +98,20 @@ function create () {
 }
 
 function update () {
-    if (cursors.left.isDown)
-    {
+    if (cursors.left.isDown) {
         player.setVelocityX(-160);
-
         player.anims.play('left', true);
-    }
-    else if (cursors.right.isDown)
-    {
+        
+    } else if (cursors.right.isDown) {
         player.setVelocityX(160);
-
         player.anims.play('right', true);
-    }
-    else
-    {
+        
+    } else {
         player.setVelocityX(0);
-
         player.anims.play('turn');
     }
 
-    if (cursors.up.isDown && player.body.touching.down)
-    {
+    if (cursors.up.isDown && player.body.touching.down) {
         player.setVelocityY(-330);
     }
 }
